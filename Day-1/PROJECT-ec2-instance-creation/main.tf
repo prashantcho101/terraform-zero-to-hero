79e8d5d35a47918dec9fd8717ca8a4a79a0c47b6
prashantcho101 <89179347+prashantcho101@users.noreply.github.com>
2024-03-18 17:43:48 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
+
+resource "aws_instance" "this" {
+  ami                     = "ami-0dcc1e21636832c5d"
+  instance_type           = "m5.large"
+}
